6938c27ae8b48bd64f051bd4ce8dbc43bc7f27f1
Nelson Green <nelson.green@gmail.com>
2024-01-29 21:34:32 -0600

    Able to simulate LEDs with iterator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -137,4 +137,10 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final class LedConstants {
+    public static final int kLedPWMPort = 0;
+    public static final int kLedCount = 512;
+
+  }
 }
