b1cdc066b45499a3d0b615156f47ade7248ac87e
Nelson Green <nelson.green@gmail.com>
2024-02-13 20:49:38 -0600

    write underroller command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -116,6 +116,7 @@ public final class Constants {
 
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
     public static final double kDriveDeadband = 0.05;
   }
 
@@ -136,5 +137,10 @@ public final class Constants {
 
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
+  }
+
+  public static final class UnderrollerConstants {
+    public static final int kFrontCanId = 21;
+    public static final int kRearCanId = 22;
   }
 }
