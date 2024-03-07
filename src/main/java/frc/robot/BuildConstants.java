dbdf0ff4e60a93c089a36e4263705b3880e19a36
Nelson Green <nelson.green@gmail.com>
2024-03-07 17:27:16 -0600

    update build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "f341ab6f8f1c6a194f228a4bdacd6eacc10feb87";
-  public static final String GIT_DATE = "2024-02-16 00:52:20 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "8668f8689520a53dc291feb231e7e8e02efa0d82";
+  public static final String GIT_DATE = "2024-03-07 18:26:26 EST";
   public static final String GIT_BRANCH = "vision";
-  public static final String BUILD_DATE = "2024-02-29 00:17:37 EST";
-  public static final long BUILD_UNIX_TIME = 1709183857295L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-07 18:26:43 EST";
+  public static final long BUILD_UNIX_TIME = 1709854003374L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
